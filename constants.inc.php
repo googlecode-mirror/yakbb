abdd150f7b22dd0c71f28c36eecd29ca66e757ed
cddude229 <cddude229@4cb63a3c-c341-0410-9270-1fa43a952361>
2008-01-19 01:30:45 +0000

    - Setting current DB version as a constant in constants.inc.php

--- constants.inc.php
+++ constants.inc.php
@@ -3,12 +3,15 @@
 if(!defined("SNAPONE")) exit;
 
 // Directory listings.
-define("CACHEDIR", "./cache/");
-define("INCLUDESDIR", "./includes/");
-define("LANGUAGEDIR", "./languages/");
-define("LIBDIR", "./library/");
-define("PLUGINSDIR", "./plugins/");
-define("SMILIESDIR", "./smilies/");
-define("TEMPLATESDIR", "./templates/");
-define("UPLOADSDIR", "./uploads/");
+define("CACHEDIR",		"./cache/");
+define("INCLUDESDIR",	"./includes/");
+define("LANGUAGEDIR",	"./languages/");
+define("LIBDIR",		"./library/");
+define("PLUGINSDIR",	"./plugins/");
+define("SMILIESDIR",	"./smilies/");
+define("TEMPLATESDIR",	"./templates/");
+define("UPLOADSDIR",	"./uploads/");
+
+// Don't edit. These are used to keep YakBB up to date
+define("CURRENTDBVERSION", 1);
 ?>
--- install/db/mysql/upgrade_sql.php
+++ install/db/mysql/upgrade_sql.php
@@ -7,10 +7,9 @@
 if(!defined("SNAPONE")) exit;
 
 class upgrade_sql {
-	private $curver = 1;
 	public function upgrade($n){
 		// $n = current version. Will fully update to final version
-		while($n <= $this->curver && is_callable(array($this, "upgradeIt".$n))){
+		while($n <= CURRENTDBVERSION && is_callable(array($this, "upgradeIt".$n))){
 			call_user_func(array($this, "upgradeIt".$n));
 			echo $n;
 			$n++;
