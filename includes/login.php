ecf6268f7d9600b1ce223a216233edca8f3f73f7
cddude229 <cddude229@4cb63a3c-c341-0410-9270-1fa43a952361>
2008-02-28 21:27:33 +0000

    - Small security fix. It may not have even been a threat. - Switched it to class based system

--- includes/login.php
+++ includes/login.php
@@ -8,67 +8,80 @@
 
 if(!defined("SNAPONE")) exit;
 
-if($guest == false && $yak->settings["switch_users"] == false){
-	$tp->error("login_logged_in");
-}
+class login {
+	function __construct(){
+		global $guest, $yak, $tp, $lang, $plugins;
 
-$tp->setTitle("login");
-$tp->loadFile("login", "login.tpl", array(
-	"errors" => array()
-));
-$lang->learn("login");
-$tp->addNav($lang->item("nav_login"));
-$plugins->callhook("login_start");
-
-
-if(isset($_REQUEST["submitit"]) && isset($_REQUEST["username"]) && isset($_REQUEST["password"])){
-	$plugins->callhook("login_form_sent");
-	$username = secure($_REQUEST["username"]);
-	$password = sha256($_REQUEST["password"]);
-
-	$tp->addVar("login", array(
-		"USER" => substr($username, 0, $yak->settings["username_max_length"])
-	));
-
-	$error = array();
-	libraryLoad("validation.lib");
-	if(empty($username)){
-		$error[] = "username_empty";
-	} else if(strlen($username) < $yak->settings["username_max_length"]){
-		$x = $db->query("SELECT password FROM ".DBPRE."users WHERE name='".$username."'");
-		if($db->numRows($x) == 1){
-			$x = $db->fetch($x);
-			if($x["password"] == $password){
-				// Login successful
-				$plugins->callhook("login_success_start");
-	
-				setcookie(DBPRE."user", $username, time()+9999999);
-				setcookie(DBPRE."pass", $password, time()+9999999);
-
-				$plugins->callhook("login_success_end");
-
-				redirect("?");
-			} else {
-				$error[] = "wrong_password";
-			}
-		} else {
-			$error[] = "username_doesnt_exist";
+		// Make sure user can login
+		if($guest == false && $yak->settings["switch_users"] == false){
+			$tp->error("login_logged_in");
 		}
-	} else {
-		$error = "username_too_long";
+
+		// Template stuff
+		$tp->setTitle("login");
+		$tp->loadFile("login", "login.tpl", array(
+			"errors" => array(),
+			"REG" => isset($_REQUEST["reg"]) // Show registration message if it's set to be shown
+		));
+		$lang->learn("login");
+		$tp->addNav($lang->item("nav_login"));
+
+		$plugins->callhook("login_start");
+
+		if(isset($_REQUEST["submitit"]) && isset($_REQUEST["username"]) && isset($_REQUEST["password"])){
+			$this->formSent();
+		}
+
+		$plugins->callhook("login_end");
 	}
 
-	// Errored
-	$error = $plugins->callhook("login_error", $error);
-	$lang->learn("errors");
-	$tp->addVar("login", array(
-		"errors" => array_map(array($lang, "item"), $error)
-	));
+	function formSent(){
+		// Form was sent
+		global $plugins, $tp, $db, $yak, $lang;
+
+		$plugins->callhook("login_form_sent");
+		$username = $db->secure(secure($_REQUEST["username"]));
+		$password = sha256($_REQUEST["password"]);
+
+		$tp->addVar("login", array(
+			"USER" => substr($username, 0, $yak->settings["username_max_length"])
+		));
+
+		$error = array();
+		libraryLoad("validation.lib");
+		if(empty($username)){
+			$error[] = "username_empty";
+		} else if(strlen($username) < $yak->settings["username_max_length"]){
+			$x = $db->query("SELECT password FROM ".DBPRE."users WHERE name='".$username."'");
+			if($db->numRows($x) == 1){
+				$x = $db->fetch($x);
+				if($x["password"] == $password){
+					// Login successful
+					$plugins->callhook("login_success_start");
+		
+					setcookie(DBPRE."user", $username, time()+9999999);
+					setcookie(DBPRE."pass", $password, time()+9999999);
+
+					$plugins->callhook("login_success_end");
+
+					redirect("?");
+				} else {
+					$error[] = "wrong_password";
+				}
+			} else {
+				$error[] = "username_doesnt_exist";
+			}
+		} else {
+			$error = "username_too_long";
+		}
+
+		// Errored
+		$error = $plugins->callhook("login_error", $error);
+		$lang->learn("errors");
+		$tp->addVar("login", array(
+			"errors" => array_map(array($lang, "item"), $error)
+		));
+	}
 }
-
-
-$tp->addVar("login", "REG", isset($_REQUEST["reg"]));
-
-$plugins->callhook("login_end");
 
 ?>
