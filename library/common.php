8d103d6eab6d461cf77f1b34be0e6598ff0b493b
cddude229 <cddude229@4cb63a3c-c341-0410-9270-1fa43a952361>
2008-05-21 13:40:36 +0000

    - Just adding a quick comment

--- library/common.php
+++ library/common.php
@@ -43,6 +43,8 @@ unset($connection); // Security reasons
 // Load settings.
 $cache->loadSettings(); // Load our settings from the database or cache
 $cache->loadGroups(); // Load our groups from the database or cache
+
+// Do SEO stuff briefly.
 if($yak->settings["seo_engine"] === true){
 	$tp->setSEO();
 }
